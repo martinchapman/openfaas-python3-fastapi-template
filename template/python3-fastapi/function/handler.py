2fe117ba96562ec5b0ed5bbce69dd6bd1335a7e4
Martin Chapman <contact@martinchapman.co.uk>
2025-08-17 20:36:10 +0100

    fix: add template handler return type

--- template/python3-fastapi/function/handler.py
+++ template/python3-fastapi/function/handler.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from .handler_types import RequestModel, ResponseModel
 
-async def handle(req: RequestModel):
+async def handle(req: RequestModel) -> ResponseModel:
     """handle a request to the function
     Args:
         req (dict): request body
